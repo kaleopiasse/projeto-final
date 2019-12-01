43ecc2a3c47d1dcd6d2c7b20118ff4c571cb9f48
Kaleo A. Piasse <kaleopiasse@gmail.com>
2019-12-01 11:08:41 -0300

    Exibe nome do usuario logado header

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  userName;
+
   constructor() { }
 
   ngOnInit() {
+    this.userName = sessionStorage.getItem('name');
   }
 
 }
